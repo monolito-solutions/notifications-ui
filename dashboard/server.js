57052327ad8b93d470576279a576934c60a4d894
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-12 10:57:41 -0500

    Fix: allow cors

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,28 +1,41 @@
-let http = require('http'),
-    fs = require('fs'),
-    path = require('path');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+app.use(cors());
 
 function serveFile(response, filePath) {
     fs.readFile(filePath, function (err, content) {
         if (err) {
-            response.writeHead(404);
-            response.end();
+            response.status(404).end();
         } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Access-Control-Allow-Origin': '*',
+            });
             response.end(content, 'utf-8');
         }
     });
 }
 
-http.createServer(function (request, response) {
-    let pathname = request.url;
-    if (pathname === '/') {
-        serveFile(response, './index.html');
-    } else if (pathname === '/create') {
-        serveFile(response, './create.html');
-    } else if (pathname === '/search') {
-        serveFile(response, './search.html');
-    } else {
-        serveFile(response, './not-found.html');
-    }
-}).listen(8000);
+app.get('/', function (req, res) {
+    serveFile(res, path.join(__dirname, 'index.html'));
+});
+
+app.get('/create', function (req, res) {
+    serveFile(res, path.join(__dirname, 'create.html'));
+});
+
+app.get('/search', function (req, res) {
+    serveFile(res, path.join(__dirname, 'search.html'));
+});
+
+app.get('*', function (req, res) {
+    serveFile(res, path.join(__dirname, 'not-found.html'));
+});
+
+app.listen(8000, function () {
+    console.log('Server listening on port 8000');
+});
