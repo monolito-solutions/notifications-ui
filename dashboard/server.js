e04024f37064843043f26e1ed142ce526c9187fc
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-12 09:52:08 -0500

    Feat: add new UI server paths

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,13 +1,28 @@
 let http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
+function serveFile(response, filePath) {
+    fs.readFile(filePath, function (err, content) {
+        if (err) {
+            response.writeHead(404);
+            response.end();
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+}
+
+http.createServer(function (request, response) {
+    let pathname = request.url;
+    if (pathname === '/') {
+        serveFile(response, './index.html');
+    } else if (pathname === '/create') {
+        serveFile(response, './create.html');
+    } else if (pathname === '/search') {
+        serveFile(response, './search.html');
+    } else {
+        serveFile(response, './not-found.html');
     }
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(8000);
-});
+}).listen(8000);
